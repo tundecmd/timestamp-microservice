8002a7334185a0f6652f27fd0e04a031f90b5a5c
Adepoju Lateef <ademustexcel@gmail.com>
2020-11-19 13:30:30 +0100

    added project files up  for submission

--- server.js
+++ server.js
@@ -7,11 +7,47 @@ const requestHandler = (req, res) => {
       if (err) {
         throw err
       }
-      
+        
       res.writeHead(200, {"Content-Type": "text/html"});
       res.end(html);
     })
-  }
+    
+  } else if (req.url.startsWith('/api/timestamp')) {
+      const dateString = req.url.split('/api/timestamp/')[1];
+      const getTimestamp = (date) => {
+        return {
+          utc: date.toUTCString(),
+          unix: date.getTime()
+        }
+      }
+      let timestamp;
+
+      if (dateString === '' || dateString === undefined || dateString.trim() === '') {
+        timestamp = getTimestamp(new Date())
+      } else {
+        const date = new Date(dateString);
+
+      if (!isNaN(date.getTime())) {
+        timestamp = getTimestamp(date);
+      } else {
+        timestamp = {
+          error: "invalid date"
+        };
+      } 
+    }
+
+       res.writeHead(200, { "Content-Type": "application/json" });
+       res.end(JSON.stringify(timestamp));
+    
+  } else {
+    fs.readFile('views/error.html', (err, errhtml) => {
+      if (err) {
+        throw err
+      }
+        
+      res.writeHead(200, {"Content-Type": "text/html"});
+      res.end(errhtml);
+  })}
 }
 
 const server = http.createServer(requestHandler);
